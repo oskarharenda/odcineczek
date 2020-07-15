5985ee8f54ebd48b7313e8b5ff01e32eb8a7b802
harendao <Oskar.Harenda@ttpsc.pl>
2020-07-15 11:55:07 +0200

    Initial commit

---
+++ src/main/java/pl/oskar/harenda/odcineczek/OdcineczekApplication.java
@@ -0,0 +1,13 @@
+package pl.oskar.harenda.odcineczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdcineczekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdcineczekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/oskar/harenda/odcineczek/OdcineczekApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oskar.harenda.odcineczek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdcineczekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
